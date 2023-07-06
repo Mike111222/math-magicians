c68f408633b0c5f585495b17e22d2bf425a6c796
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-06 14:39:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-
 import React from 'react';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="app">
-      <h1>Calculator App</h1>
-      <Calculator />
+      <div>
+        <h2>Calculator</h2>
+        <Calculator />
+      </div>
+      <Quote />
     </div>
   );
 }
