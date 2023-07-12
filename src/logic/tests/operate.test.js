fe54118966f29afebbd6cf038bc80b234ed81efb
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-12 22:07:33 +0500

    Update operate.test.js

--- src/logic/tests/operate.test.js
+++ src/logic/tests/operate.test.js
@@ -1,17 +1,35 @@
 import operate from '../operate';
 
 describe('Check calculator operations are as expected', () => {
-  test('sum', () => {
-    expect(operate(10, 4, '+')).toEqual('14');
+  test('should perform addition operation', () => {
+    expect(operate('10', '4', '+')).toEqual('14');
   });
-  test('substract', () => {
-    expect(operate(18, 6, '-')).toEqual('12');
-  }); test('multiply', () => {
-    expect(operate(5, 3, 'x')).toEqual('15');
-  }); test('divide', () => {
-    expect(operate(32, 8, '÷')).toEqual('4');
+
+  test('should perform subtraction operation', () => {
+    expect(operate('18', '6', '-')).toEqual('12');
   });
-  test('mod', () => {
-    expect(operate(21, 5, '%')).toEqual('1');
+
+  test('should perform multiplication operation', () => {
+    expect(operate('5', '3', 'x')).toEqual('15');
+  });
+
+  test('should perform division operation', () => {
+    expect(operate('32', '8', '÷')).toEqual('4');
+  });
+
+  test('should handle division by zero', () => {
+    expect(operate('10', '0', '÷')).toEqual("Can't divide by 0.");
+  });
+
+  test('should perform modulo operation', () => {
+    expect(operate('21', '5', '%')).toEqual('1');
+  });
+
+  test('should handle modulo with zero divisor', () => {
+    expect(operate('10', '0', '%')).toEqual("Can't find modulo as can't divide by 0.");
+  });
+
+  test('should throw an error for an unknown operation', () => {
+    expect(() => operate('5', '3', '^')).toThrow("Unknown operation '^'");
   });
 });
